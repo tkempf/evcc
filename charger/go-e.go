255179156ef886e8ba1f1ff47d19e59a8ec5b618
Carsten Thiele <software@carsten-thiele.de>
2020-11-21 10:29:44 +0100

    Switch to go-e amx api (#460) replaces #457

--- charger/go-e.go
+++ charger/go-e.go
@@ -28,8 +28,7 @@ type goeStatusResponse struct {
 	Fwv string `json:"fwv"`        // firmware version - indicates local response
 	Car int    `json:"car,string"` // car status
 	Alw int    `json:"alw,string"` // allow charging
-	Amp int    `json:"amp,string"` // current [A], goes to EEPROM
-	Amx int    `json:"amx,string"` // current [A], doesn't go to EEPROM
+	Amp int    `json:"amp,string"` // current [A]
 	Err int    `json:"err,string"` // error
 	Stp int    `json:"stp,string"` // stop state
 	Tmp int    `json:"tmp,string"` // temperature [°C]
@@ -202,9 +201,9 @@ func (c *GoE) Enable(enable bool) error {
 
 // MaxCurrent implements the Charger.MaxCurrent interface
 func (c *GoE) MaxCurrent(current int64) error {
-	status, err := c.apiUpdate(fmt.Sprintf("amx=%d", current))
-	if err == nil && isValid(status) && int64(status.Amx) != current {
-		return fmt.Errorf("amx update failed: %d", status.Amx)
+	status, err := c.apiUpdate(fmt.Sprintf("amx=%d", current)) // setting amx is visible in amp response without hitting EEPROM
+	if err == nil && isValid(status) && int64(status.Amp) != current {
+		return fmt.Errorf("amx update failed: %d", status.Amp)
 	}
 
 	return err
